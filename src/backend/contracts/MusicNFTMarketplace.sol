93cf9f9c82550811312a199d966f697d0d465d74
AMAN MISHRA <amankumar2021ap@gmail.com>
2023-07-01 04:32:21 +0530

    local deploy script done

--- src/backend/contracts/MusicNFTMarketplace.sol
+++ src/backend/contracts/MusicNFTMarketplace.sol
@@ -8,5 +8,39 @@ import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 contract MusicNFTMarketplace is ERC721("DAppFi","DAPP"),Ownable{
 string public baseURI =
         "https://bafybeidhjjbjonyqcahuzlpt7sznmh4xrlbspa3gstop5o47l6gsiaffee.ipfs.nftstorage.link/";
+string public baseExtension=".json";
+address public artist;
+uint256 public royaltyFee;
+
+ struct MarketItem{
+      uint256 tokenId;
+      address payable seller;
+      uint256 price;
+ }
+ MarketItem[] public marketItems;
+ 
+
+/* In constructor we initalize royalty fee, artist address and prices of music nfts*/
+    constructor(
+        uint256 _royaltyFee,
+        address _artist,
+        uint256[] memory _prices
+    ) payable {
+        require(
+            _prices.length * _royaltyFee <= msg.value,
+            "Deployer must pay royalty fee for each token listed on the marketplace"
+        );
+         artist = _artist;
+         royaltyFee = _royaltyFee;
+
+        for (uint8 i = 0; i < _prices.length; i++) {
+            require(_prices[i] > 0, "Price must be greater than 0");
+            _mint(address(this), i);
+            marketItems.push(MarketItem(i, payable(msg.sender), _prices[i]));
+        }
+    }
+
+
+
 
 }
